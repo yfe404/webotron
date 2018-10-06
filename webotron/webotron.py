9eeee03228f8b657b275dcae3eb1ae9cc43dcd20
yafeunteun <yannfeunteun@gmail.com>
2018-10-06 21:53:42 +0200

    feat: sync local directory to a s3 bucket

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,5 +1,7 @@
 import boto3
 import click
+import mimetypes
+from pathlib import Path
 from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name="adminuser")
@@ -72,9 +74,34 @@ def setup_bucket(bucket):
     return
     
 
-    
+def upload_file(s3_bucket, path, key):
 
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+        'ContentType': content_type
+    })
+    
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    """ Sync contents of PATHNAME to BUCKET """
+
+    s3_bucket = s3.Bucket(bucket)
+    
+    root = Path(pathname).expanduser().resolve()
+    
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root))) 
+
+    handle_directory(root)
+                
     
 if __name__ == "__main__":
     cli()
